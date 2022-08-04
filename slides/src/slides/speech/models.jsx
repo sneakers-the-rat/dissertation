650ca3e82933570cbf9a21defeb44f4acfa63e21
sneakers-the-rat <JLSaunders987@gmail.com>
2022-08-03 22:03:48 -0700

    speech

--- slides/src/slides/speech/models.jsx
+++ slides/src/slides/speech/models.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Markdown, Heading, Appear, Image, FlexBox, Text} from 'spectacle';
+import {Markdown, Heading, Appear, Image, FlexBox, Text, Box, Quote, Link} from 'spectacle';
 import SvgAnimator from '../../components/svg_animator';
 import chair from '../../img/chair.svg';
+
+import {Citations, Citation} from '../../components/typography.jsx';
 
 import {FancyHeading} from '../../components/styled.jsx';
 import {PositionedHeading} from '../../components/basics.jsx';
@@ -10,6 +12,13 @@ import baby_rumbly from '../../img/baby_rumbly.png';
 import soundwave from '../../img/soumdwave.png';
 import vot from '../../img/vot.png';
 import qb from '../../img/cuebased-01.svg';
+import mtf from '../../img/mtf.png';
+
+import mtf3hz from '../../assets/sound/mtf_3Hz.mp3';
+import mtf12hz from '../../assets/sound/mtf_12Hz.mp3';
+import mtf4cyc from '../../assets/sound/mtf_4cyc.mp3';
+import mtf05cyc from '../../assets/sound/mtf_05cyc.mp3';
+import mtforig from '../../assets/sound/mtf_orig.mp3';
 
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { Icon } from '@mui/material';
@@ -24,7 +33,7 @@ export const HaskinsModel = (
       <FancyHeading>
         "Cue-Based" Models
       </FancyHeading>
-      <Image position={"absolute"} top={0} src={qb}/>
+      <Image id={"cuebased"} position={"absolute"} top={0} left={0} src={qb} width={1920} height={1080}/>
 
 
     </div>
@@ -112,6 +121,44 @@ export const Chair = (
     </div>
 );
 
+export const NoCues = (
+    <div>
+      <Box width={2/5} margin={'40px 40px'}>
+        <Quote fontSize={"3em"} fontFamily={"Source Serif Pro"} fontWeight={200} fontStyle={"italic"}>
+          "Question: Which acoustic elements are essential for the perception of speech?"<br/>
+          "Answer: None"<br/>
+          <Text fontSize={"0.5em"}>Philip Rubin, Robert Remez, Jennifer Pardo of
+            <Link fontSize={"1em"}  href={"https://web.archive.org/web/20200809223413/http://www.haskins.yale.edu/featured/sws/sws.html"}>
+              Haskins Labs
+            </Link>
+          </Text>
+        </Quote>
+      </Box>
+      <Box margin={"auto"}>
+        <Image style={{display: 'block', margin:"auto"}} src={mtf}></Image>
+      </Box>
+      <audio controls style={{position:"absolute", top:"50%", right:"20px"}}>
+        <source src={mtf3hz} type={"audio/mpeg"}/>
+      </audio>
+      <audio controls style={{position:"absolute", top:"75%", right:"20px"}}>
+        <source src={mtf12hz} type={"audio/mpeg"}/>
+      </audio>
+      <audio controls style={{position:"absolute", top:"50%", left:"20px"}}>
+        <source src={mtf05cyc} type={"audio/mpeg"}/>
+      </audio>
+      <audio controls style={{position:"absolute", top:"75%", left:"20px"}}>
+        <source src={mtf4cyc} type={"audio/mpeg"}/>
+      </audio>
+      <Citations>
+      <Citation author={"Elliott & Theunissen"}
+      title={"The Modulation transfer function for speech intelligibility"}
+      link={"https://doi.org/10.1371/journal.pcbi.1000302"}
+      year={"2009"}/>
+      </Citations>
+
+    </div>
+)
+
 export const InverseModel = (
     <Markdown>
       {`
@@ -129,6 +176,7 @@ export const InverseModel = (
 const Slides = [
     HaskinsModel,
     Chair,
+    NoCues,
     InverseModel
 ]
 
